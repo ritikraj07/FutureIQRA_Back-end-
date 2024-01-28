40d449be4a952f97eb14a82d63dd40eecd4e3192
ritikraj07 <ritikra3rrr@gmail.com>
2024-01-28 23:05:19 +0530

    paymnet

--- index.js
+++ index.js
@@ -19,20 +19,20 @@ app.use(express.json());
 // app.use(express.static('static'));
 app.use(cors());
 
-// app.use((req, res, next) => {
-//     // how to use this url also https://futureiqra.onrender.com/ this my backend url
-//   res.header('Access-Control-Allow-Origin', 'https://www.futureiqra.in');  // frontend url
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); 
-  
-//   next();
-// });
-
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.futureiqra.in');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+    // how to use this url also https://futureiqra.onrender.com/ this my backend url
+  res.header('Access-Control-Allow-Origin', 'https://www.futureiqra.in');  // frontend url
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); 
+  
+  next();
 });
+
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://www.futureiqra.in');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
 
 
 app.use(morgan('tiny'))
